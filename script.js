ab415a4cbc4d220d4d14a9d14e5b61ecbbfb1606
mugeshi <nicolemugeshi@gmail.com>
2024-06-06 22:19:23 +0300

    functionality cross-off

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.querySelector('#push').onclick = function(){
     // Adding validation for empty input field
     if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-    //Adding a new task
-    else{
+        alert("Please Enter a Task");
+    } else {
+        // Adding a new task
         document.querySelector('#tasks').innerHTML += `
             <div class="task">
-                <span id="taskname">
+                <span class="taskname">
                     ${document.querySelector('#newtask input').value}
                 </span>
                 <button class="delete">
@@ -15,10 +14,20 @@ document.querySelector('#push').onclick = function(){
                 </button>
             </div>
         `;
+        
+        // Adding delete functionality to newly added tasks
         var current_tasks = document.querySelectorAll(".delete");
         for(var i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
                 this.parentNode.remove();
+            }
+        }
+
+        // Adding functionality to cross off a completed task
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
             }
         }
     }
